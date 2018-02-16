d91f35f5165853c1603e88728e08f552f2e0b46b
Eric Swotinsky <eric.swotinsky@gmail.com>
2018-02-16 13:22:42 -0800

    add HomeController tests.

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Program should check a multiple-character word against a string of words, return
 Program should ignore capitalization for purposes of finding matches between inputted word and inputted sentence.
     Example input: "ThE", "I am the walrus"
     Example output: 1
-    
+
 
 Program should only count full-word matches, not partial ones (i.e. ignoring when the first string passed in is contained within a word in the second string).
     Example input: "the", "Bring the thesaurus to the bookshelf"
---
+++ RepeatCounters.Tests/ControllerTests/HomeControllerTest.cs
@@ -0,0 +1,37 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using RepeatCounters.Controllers;
+using RepeatCounters.Models;
+
+namespace RepeatCounters.Tests
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+      [TestMethod]
+       public void Index_ReturnsCorrectView_True()
+       {
+           HomeController controller = new HomeController();
+
+           IActionResult indexView = controller.Index();
+           ViewResult result = indexView as ViewResult;
+
+           Assert.IsInstanceOfType(result, typeof(ViewResult));
+       }
+
+       [TestMethod]
+       public void Index_HasCorrectModelType_RepeatCounterObj()
+       {
+         HomeController controller = new HomeController();
+         IActionResult actionResult = controller.Index();
+         ViewResult indexView = controller.Index() as ViewResult;
+
+         var result = indexView.ViewData.Model;
+
+         Assert.AreNotEqual(result, null);
+         Assert.IsInstanceOfType(result, typeof(RepeatCounter));
+       }
+    }
+}
